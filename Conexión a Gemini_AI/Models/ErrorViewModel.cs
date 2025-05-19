22057c947b5fe1b0565111b3762e149509a5b2e0
Esteban1211 <spellman3@live.com>
2025-05-18 23:06:07 -0400

    Add project files.

---
+++ Conexión a Gemini_AI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Conexión_a_Gemini_AI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
